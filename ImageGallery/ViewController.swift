425bc30775e7d5ea4a670fe4c424e12dff30422d
AnittasSunny <53472743+AnittasSunny@users.noreply.github.com>
2021-12-02 18:05:46 -0500

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Anitta Sunny on 2021-12-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
